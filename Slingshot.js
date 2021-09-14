77211baee682d54f1bcf5d8da6b4f2bcdea2c1d4
Kanwar-Ashish-Singh <75555437+Kanwar-Ashish-Singh@users.noreply.github.com>
2021-09-14 17:01:25 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,65 @@
+/*class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.chain = Matter.Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    
+}*/
+
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    attach(body){
+    this.sling.bodyA = body;
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+
+    display(){
+     
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+          
+            
+            stroke(48,22,8);
+            
+                strokeWeight(7);
+                line(pointA.x - 20, pointA.y, pointB.x -10, pointB.y);
+          
+            
+           
+           
+
+        }
+    }
+    
+}
